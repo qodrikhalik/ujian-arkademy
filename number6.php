5b6176425dfd93f6c6efc7809ccd89a4ec6bb835
qodrikhalik <50544242+qodrikhalik@users.noreply.github.com>
2019-08-31 22:55:15 +0700

    Add files via upload

---
+++ number6.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Number 6</title>
+</head>
+<body>
+
+<table border="1">
+	<thead>
+		<th>name</th>
+		<th>work</th>
+		<th>salary</th>
+	</thead>
+
+	<tbody>
+		<td>Rebecca</td>
+		<td>Fronend Dev</td>
+		<td>10.000.000</td>
+	</tbody>
+</table>
+
+</body>
+</html>
